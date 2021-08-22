6739c281d1cbba1d86a0eee531f9b0c562cd21d4
Arasefe <ismail_yildirim69@yahoo.com>
2021-08-21 22:11:00 -0500

    21 August 2021

---
+++ src/lambdaLinkedin/LambdaIntro10.java
@@ -0,0 +1,33 @@
+package lambdaLinkedin;
+
+import java.math.BigInteger;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.UnaryOperator;
+
+public class LambdaIntro10 {
+    public static void main(String[] args) {
+        IntFunction<String>intToString=num->Integer.toString(num);
+        System.out.println("Expected value 3, actual value: "+intToString.apply(123).length());
+
+        // Static method reference using ::
+        IntFunction<String>intToString2=Integer::toString;
+        System.out.println("Expected value 4, actual value: "+intToString2.apply(4567).length());
+
+        // Lambdas made using a constructor
+        Function<String, BigInteger>newBigInt=BigInteger::new;
+        System.out.println("Expected value: 123456789, actual value: "+newBigInt.apply("123456789"));
+
+        // Example of a Lambda made from an instance method
+        Consumer<String>print= System.out::println;
+        print.accept("Coming to you directly from a lambda...");
+
+        //Unary operator
+        UnaryOperator<String>makeGreeting="Hello, "::concat;
+        System.out.println(makeGreeting.apply("Peggy"));
+
+
+
+    }
+}
---
+++ src/lambdaLinkedin/LambdaIntro11.java
@@ -0,0 +1,36 @@
+package lambdaLinkedin;
+
+import java.awt.print.Book;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaIntro11 {
+    public static void main(String[] args) {
+        List<String>names= Arrays.asList("Aras","Tulpar","Banu","Ismail");
+
+        // way to sort prior to Java 8 lambdas
+
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return b.compareTo(a);
+            }
+        });
+
+        // First iteration with Lambda
+        Collections.sort(names,(String a, String b)->{
+            return b.compareTo(a);
+        });
+
+        // Now remove the data types and allow the compile to infer the type
+        Collections.sort(names,(a,b)->b.compareTo(a));
+
+
+
+
+
+
+    }
+}
